4720bbb64f3ca90896e190b72fc68ce7fbfc9677
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-14 13:35:34 +0200

    query 2 completata

--- query.sql
+++ query.sql
@@ -1,4 +1,12 @@
 --1. Selezionare tutti gli studenti nati nel 1990 (160)
-SELECT * FROM `students`
+SELECT * 
+FROM `students`
 WHERE YEAR(`date_of_birth`) =1990
 ORDER BY `date_of_birth` ASC;
+
+-- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
+
+SELECT * 
+FROM `courses` 
+WHERE `cfu` > 10
+ORDER BY `cfu`ASC;

2ea464b2a683104f838c4d1d91d454d493198c42
DarioEsposito89 <darioesposito.work@gmail.com>
2023-09-14 16:07:47 +0200

    query 4 e 6 completate

--- query.sql
+++ query.sql
@@ -15,3 +15,15 @@ SELECT *, DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),`date_of_birth`)), '%Y') + 0 AS `
 FROM `students`
 WHERE  DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),`date_of_birth`)), '%Y') + 0  > 30
 ORDER BY `date_of_birth`DESC;
+
+--4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT * 
+FROM `courses`
+WHERE `period` LIKE "I %"
+AND `year`= 1
+ORDER BY `name` ASC;
+
+--6. Selezionare tutti i corsi di laurea magistrale (38)
+SELECT * 
+FROM `degrees`
+WHERE `level` = "magistrale";
